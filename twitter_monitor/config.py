82bf205b8a44e4b6bdd9120a742432a052f54d48
serkef <sergiokef@gmail.com>
2019-12-02 21:03:48 +0100

    Create module twitter_monitor

---
+++ twitter_monitor/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+# Screenshots
+STORE_SCREENSHOTS = True
+
+# Export location
+EXPORT_ROOT = Path(__file__).parent / "data"
---
+++ twitter_monitor/utilities.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from selenium import webdriver
+
+
+def get_screenshot(tweet_id: str, output_directory: Path) -> None:
+    """ Gets a screenshot from a tweet."""
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+
+    driver = webdriver.Remote(
+        command_executor="https://chrome.browserless.io/webdriver",
+        desired_capabilities=chrome_options.to_capabilities(),
+    )
+
+    driver.get(f"https://twitter.com/i/web/status/{tweet_id}")
+    driver.save_screenshot(str(output_directory / f"{tweet_id}.png"))
+    driver.quit()
