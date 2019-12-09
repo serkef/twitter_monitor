e7c91b9493770b8006391c5d04c55a6399f6451b
serkef <sergiokef@gmail.com>
2019-12-09 01:25:39 +0100

    Add webhook deployment script

---
+++ scripts/webhook-redeploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+curPid=$(ps -ef | grep "python" | grep "twitter" | grep "monitor" | awk '{print $2}')
+
+echo "Cloning last master"
+rm -rf ${APP_HOME}
+git clone --quiet https://github.com/serkef/twitter_monitor.git ${APP_HOME}
+cp ${APP_HOME}/scripts/webhook-redeploy.sh $(realpath $0)
+
+echo "Installing dependencies"
+python3 -m venv ${APP_ENV}
+. ${APP_ENV}/bin/activate
+pip install -qqq wheel
+pip install -qqq --upgrade -r ${APP_HOME}/requirements.txt
+
+# Start the process again
+echo "Starting monitor"
+nohup python3 ${APP_HOME}/twitter_monitor/monitor.py >> ${APP_LOGS}/twitter_monitor.$(date "+%Y%m%d%H%M%S").log 2>&1 &
+echo "Killing previous version"
+kill curPid
