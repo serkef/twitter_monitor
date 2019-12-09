9b7924e9ce752fb69f2c4c39d9ba6a3a0612e2de
serkef <sergiokef@gmail.com>
2019-12-09 14:55:47 +0100

    Fix path

--- twitter_monitor/config.py
+++ twitter_monitor/config.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
@@ -7,4 +9,4 @@ load_dotenv(find_dotenv())
 STORE_SCREENSHOTS = True
 
 # Export location
-EXPORT_ROOT = os.getenv('EXPORT_ROOT')
+EXPORT_ROOT = Path(os.getenv('EXPORT_ROOT'))
