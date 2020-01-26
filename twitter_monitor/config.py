17346dce8c1bc85a520722dc2f6ee2037d70005d
serkef <sergiokef@gmail.com>
2020-01-26 14:43:27 +0100

    Fix config option

--- twitter_monitor/config.py
+++ twitter_monitor/config.py
@@ -8,7 +8,7 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 # Screenshots
-STORE_SCREENSHOTS = os.getenv("STORE_SCREENSHOTS", True)
+STORE_SCREENSHOTS = bool(os.getenv("STORE_SCREENSHOTS", True))
 
 # Export location
 EXPORT_ROOT = Path(os.getenv("EXPORT_ROOT"))
