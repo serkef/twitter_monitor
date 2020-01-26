3a788f0bdf512bf3b671248df69de49ace7dfc57
serkef <sergiokef@gmail.com>
2020-01-26 17:20:12 +0100

    Add API method for screenshots

--- twitter_monitor/utilities.py
+++ twitter_monitor/utilities.py
@@ -1,25 +1,28 @@
 """ custom utilities """
-
+import json
 from pathlib import Path
 
+import requests
 from selenium import webdriver
 
 from config import BROWSERLESS_TOKEN
 
 
 def get_screenshot(*args, **kwargs) -> None:
+    """ Gets a screenshot from a tweet """
+
     get_screenshot_selenium(*args, **kwargs)
 
 
 def get_screenshot_selenium(tweet_id: str, output_directory: Path) -> None:
-    """ Gets a screenshot from a tweet """
+    """ Takes a screenshot using selenium with browserless engine """
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     capabilities = chrome_options.to_capabilities()
     if BROWSERLESS_TOKEN:
-        capabilities['browserless.token'] = BROWSERLESS_TOKEN
+        capabilities["browserless.token"] = BROWSERLESS_TOKEN
 
     driver = webdriver.Remote(
         command_executor="https://chrome.browserless.io/webdriver",
@@ -33,4 +36,21 @@ def get_screenshot_selenium(tweet_id: str, output_directory: Path) -> None:
 
 
 def get_screenshot_api(tweet_id: str, output_directory: Path) -> None:
-    pass
+    """ Takes a screenshot calling the browserless screenshot API """
+
+    api_url = f"https://chrome.browserless.io/screenshot"
+    params = {"token": BROWSERLESS_TOKEN}
+    headers = {"Cache-Control": "no-cache", "Content-Type": "application/json"}
+    payload = {
+        "url": f"https://twitter.com/i/web/status/{tweet_id}",
+        "options": {"fullPage": True, "type": "jpeg", "quality": 5},
+    }
+    response = requests.post(
+        api_url, headers=headers, params=params, data=json.dumps(payload)
+    )
+    try:
+        response.raise_for_status()
+        with open(output_directory / f"{tweet_id}.jpg", "wb") as fout:
+            fout.write(response.content)
+    except Exception as exc:
+        print("Can't save screenshot.", exc)
