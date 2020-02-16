311ba22f58891427fac9f4a414a1422e8c5e6349
serkef <sergiokef@gmail.com>
2020-02-16 18:27:46 +0100

    Improve redeploy script

--- scripts/webhook-redeploy.sh
+++ scripts/webhook-redeploy.sh
@@ -8,13 +8,13 @@ git clone --quiet https://github.com/serkef/twitter_monitor.git ${APP_HOME}
 cp ${APP_HOME}/scripts/* $(dirname $0)
 
 echo "Installing dependencies"
-python3 -m venv ${APP_ENV}
+python3.7 -m venv ${APP_ENV}
 . ${APP_ENV}/bin/activate
-pip install -qqq wheel
-pip install -qqq --upgrade -r ${APP_HOME}/requirements.txt
+cd ${APP_HOME}
+poetry install --no-dev
 
 # Start the process again
 echo "Starting monitor"
-nohup python3 ${APP_HOME}/twitter_monitor/monitor.py &
+nohup python3.7 ${APP_HOME}/twitter_monitor/monitor.py &
 echo "Killing previous version"
 kill ${curPid}
